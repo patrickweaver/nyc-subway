e02b8b7aa2a62a6cb364d2607f8ed12dd597bdc5
Patrick Weaver <pjpwea@gmail.com>
2020-01-11 17:07:00 -0500

    make all routes default

--- server/server.js
+++ server/server.js
@@ -5,19 +5,23 @@ app.use(express.static('server/public'));
 const rp = require('request-promise');
 const GtfsRealtimeBindings = require('gtfs-realtime-bindings');
 
-async function getFeed(feedId) {
+async function getFeed(feedId=false) {
   
   var options = {
     uri: 'http://datamine.mta.info/mta_esi.php',
     qs: {
       key: process.env.MTA_API_KEY,
-      feed_id: feedId
     },
     encoding: null,
     headers: {
       'User-Agent': 'Request-Promise'
     }
   };
+
+  // Default is data from all routes
+  if (feedId) {
+    options.qs.feed_id = feedId;
+  }
 
   try {
     const feedResponse = await rp(options);
@@ -35,8 +39,6 @@ app.get('/api/g', async function(req, res) {
   const feedResponse = await getFeed(31);
   res.send(feedResponse);
 });
-
-
 
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
