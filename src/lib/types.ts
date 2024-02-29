07dd178201e3ee3c98d9a8dfab4763f18f976236
Patrick Weaver <pjpwea@gmail.com>
2024-02-29 14:45:27 -0500

    Combine all data on server

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,22 +1,36 @@
+import GtfsRealtimeBindings from 'gtfs-realtime-bindings';
+
 export type ApiResponseError = {
-    message: string;
-}
+	message: string;
+};
 
 export type ApiResponseBody = {
-    message: string;
-    success: boolean;
-    error: ApiResponseError | null;
-    data: unknown | null;
-}
+	message: string;
+	success: boolean;
+	error: ApiResponseError | null;
+	data: unknown | null;
+};
 
 export const LineGroup = {
-    ACE: "ace",
-	BDFM: "bdfm",
-	G: "g",
-	JZ: "jz",
-	NQRW: "nqrw",
-	L: "l",
-	ONE_TWO_THREE_FOUR_FIVE_SIX_GS: "123456",
-	SEVEN: "7",
-	SIR: "si"
-}
+	ACE: 'ace',
+	BDFM: 'bdfm',
+	G: 'g',
+	JZ: 'jz',
+	NQRW: 'nqrw',
+	L: 'l',
+	ONE_TWO_THREE_FOUR_FIVE_SIX_GS: '123456',
+	SEVEN: '7',
+	SIR: 'si'
+};
+
+export type ParsedTripData = {
+	tripId: string;
+	trip: GtfsRealtimeBindings.transit_realtime.ITripDescriptor | null | undefined;
+	stopTimeUpdates:
+		| GtfsRealtimeBindings.transit_realtime.TripUpdate.IStopTimeUpdate[]
+		| null
+		| undefined;
+	currentStopSequence?: number | null;
+	vehicleTimestamp?: string | null;
+	stopId?: string | null;
+};
--- src/routes/api/[line]/+server.ts
+++ src/routes/api/[line]/+server.ts
@@ -1,39 +1,33 @@
-import type { RequestHandler } from './$types'
-import { isErrorWithMessage, isZodValidationError } from '$lib'
-import { type ApiResponseBody } from '$lib/types'
-import { LineGroupEnum } from '$lib/validation'
+import type { RequestHandler } from './$types';
+import { isErrorWithMessage, isZodValidationError } from '$lib';
+import { type ApiResponseBody } from '$lib/types';
+import { LineGroupEnum } from '$lib/validation';
+import { getParsedTripData } from '$lib/mtaApi';
 
-import { MTA_API_KEY, TIMEZONE, UPDATE_FREQUENCY_IN_SECONDS } from '$env/static/private'
-
-if (
-	!MTA_API_KEY ||
-	!TIMEZONE ||
-	!UPDATE_FREQUENCY_IN_SECONDS
-	) {
-	console.log("Fatal Error: Missing ENV variable.");
-	process.exit();
-}
-
-export const GET: RequestHandler = ({ params }) => {
-    try {
-        const requestTime = new Date().getTime();
-        const line = LineGroupEnum.parse(params.line)
-        const data = {
-            requestTime,
-            line
-        }
+export const GET: RequestHandler = async ({ params }) => {
+	try {
+		const requestTime = new Date().getTime();
+		const line = LineGroupEnum.parse(params.line);
+		const feedData = await getParsedTripData(line);
+		const data = {
+			requestTime,
+			line,
+			feedData,
+			count: feedData.length
+		};
 		const responseBody: ApiResponseBody = {
 			message: 'NYC Subway',
 			success: true,
 			error: null,
 			data
-		}
+		};
 
 		return new Response(JSON.stringify(responseBody));
 	} catch (_error: unknown) {
-		let errorMessage = "Server error"
+		console.log({ _error });
+		let errorMessage = 'Server error';
 		if (isZodValidationError(_error)) {
-			errorMessage = _error.issues[0].message
+			errorMessage = _error.issues[0].message;
 		} else if (isErrorWithMessage(_error)) {
 			errorMessage = _error.message;
 		}
@@ -44,7 +38,7 @@ export const GET: RequestHandler = ({ params }) => {
 				message: errorMessage
 			},
 			data: null
-		}
-		return new Response(JSON.stringify(errorResponseBody))
+		};
+		return new Response(JSON.stringify(errorResponseBody));
 	}
-}
+};
